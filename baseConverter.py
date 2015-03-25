ccbf193669adcfb6672fedcfc3e88329e8391da8
Adishesh <adisheshpantec@yahoo.co.in>
2015-03-25 19:41:57 +0530

    cleanup

--- baseConverter.py
+++ baseConverter.py
@@ -1,7 +1,3 @@
-mapping = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-#mapping = [' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-#mapping = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-
 def baseSystemToN(decimal,destSymbols):    
     base = len(destSymbols)
     num = []
@@ -23,11 +19,6 @@ def toSystemBase(s,sourceSymbols):
     return result
 
 def baseXtoY(source,sourceSymbols,destSymbols):
-        return baseSystemToN(
-                toSystemBase(source,sourceSymbols),
-                destSymbols )
-
-base2_symbols = ['0','1']
-base10_symbols = ['0','1','2','3','4','5','6','7','8','9']
-
-print(baseXtoY('1111011',sourceSymbols = base2_symbols, destSymbols = base10_symbols))
+    return baseSystemToN(
+             toSystemBase(source,sourceSymbols),
+             destSymbols )
