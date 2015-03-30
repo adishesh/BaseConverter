b4c86881dc8ca51b4696eb73832c30df59b804dd
Adishesh <adisheshpantec@yahoo.co.in>
2015-03-31 00:42:57 +0530

    formatting, variable renames

--- baseConverter.py
+++ baseConverter.py
@@ -1,24 +1,40 @@
-def baseSystemToN(decimal,destSymbols):    
-    base = len(destSymbols)
+def baseSystemTobaseN(n,outputBaseSymbols):    
+    base = len(outputBaseSymbols)
     num = []
     while True:
-        q = decimal//base
-        r = decimal % base
+        q = n//base
+        r = n % base
         num.append(r)
-        decimal = q
+        n = q
         if q < base:
             num.append(q)
             break
-    return ''.join([destSymbols[x] for x in num[::-1]])
+    return ''.join([outputBaseSymbols[x] for x in num[::-1]])
 
-def toSystemBase(s,sourceSymbols):
-    base = len(sourceSymbols)   
+def toSystemBase(s,inputBaseSymbols):
+    base = len(inputBaseSymbols)   
     result = 0
     for i,j in enumerate(s[::-1]):
-        result += sourceSymbols.index(j)*pow(base,i)
+        result += inputBaseSymbols.index(j)*pow(base,i)
     return result
 
-def baseXtoY(source,sourceSymbols,destSymbols):
-    return baseSystemToN(
-             toSystemBase(source,sourceSymbols),
-             destSymbols )
+def baseXtoY(source,inputBaseSymbols,outputBaseSymbols):
+    return baseSystemTobaseN(
+             toSystemBase(source,inputBaseSymbols),
+             outputBaseSymbols )
+
+
+"""
+# Examples
+
+base10 = ['0','1','2','3','4','5','6','7','8','9']
+
+base26 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+          'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+          'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+print( baseXtoY( '12314123',
+                 inputBaseSymbols = base10 ,
+                 outputBaseSymbols = base26) )
+
+"""
